c0f7dd4b5b10b20b5b009aa75d89e39fde0de200
Redminer9630 <robertchalupka5@gmail.com>
2025-04-08 14:17:27 +0200

    Update renderer.js

--- renderer.js
+++ renderer.js
@@ -1,17 +1,18 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const categories = document.querySelectorAll('.category');
+function loadPage(url) {
+    fetch(url)
+        .then(response => {
+            if (!response.ok) { throw new Error('Seite nicht gefunden'); }
+            return response.text();
+        })
+        .then(data => { document.body.innerHTML = data; })
+}
 
-    categories.forEach(category => {
-        category.addEventListener('click', () => {
-            const name = category.alt.toLowerCase().replace(/ /g, '');
-            const url = `app/${name}/${name}.html`;
+const categories = document.querySelectorAll('.category');
 
-            fetch(url)
-                .then(response => {
-                    if (!response.ok) { throw new Error('Seite nicht gefunden'); }
-                    return response.text();
-                })
-                .then(html => { document.body.innerHTML = html; })
-        });
+categories.forEach(category => {
+    category.addEventListener('click', () => {
+        const path = category.dataset.path;
+        const url = `app/${path}/${path}.html`;
+        loadPage(url);
     });
 });
