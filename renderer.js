aecd65275a7bc3c7d07098320a16f67b6721dc7c
Redminer9630 <robertchalupka5@gmail.com>
2025-04-08 14:13:59 +0200

    Update renderer.js

--- renderer.js
+++ renderer.js
@@ -1,1 +1,17 @@
-document.getElementById('mclauncher').addEventListener('click', () => { loadPage('app/mclauncher/launcher.html'); });
+document.addEventListener('DOMContentLoaded', () => {
+    const categories = document.querySelectorAll('.category');
+
+    categories.forEach(category => {
+        category.addEventListener('click', () => {
+            const name = category.alt.toLowerCase().replace(/ /g, '');
+            const url = `app/${name}/${name}.html`;
+
+            fetch(url)
+                .then(response => {
+                    if (!response.ok) { throw new Error('Seite nicht gefunden'); }
+                    return response.text();
+                })
+                .then(html => { document.body.innerHTML = html; })
+        });
+    });
+});
